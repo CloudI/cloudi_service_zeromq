0c62d5b3cdca184bac4e3e3d10c2a5208400d0aa
Michael Truog <mjtruog@gmail.com>
2014-10-30 20:00:41 -0700

    Update hex package.

---
+++ mix.exs
@@ -0,0 +1,32 @@
+defmodule CloudIServiceZeromq do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_zeromq,
+     version: "1.4.0-rc.4",
+     language: :erlang,
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:erlzmq,
+      [git: "https://github.com/zeromq/erlzmq2.git",
+       branch: "master"]},
+     {:cloudi_core, "~> 1.4.0-rc.4"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework ZeroMQ Service"
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_zeromq"}]
+   end
+end
